77f614f20ccaf283cb4340409436870cc938db66
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-10-21 12:26:49 +0100

    Implemented Intro component #3

--- components/intro.tsx
+++ components/intro.tsx
@@ -27,90 +27,122 @@ export default function Intro() {
   };
 
   return (
-    <section>
-      <div className="flex flex-col gap-y-4 w-full items-center justify-center">
-        <div className='relative'>
-          <Image
-            alt="profil"
-            src="/profil.png"
-            height="100"
-            width="100"
-            quality="100"
-            priority={true}
-            className="h-36 w-36 object-cover rounded-full border-[0.35rem] border-white shadow-lg"
-          ></Image>
-          <span className='text-4xl absolute bottom-0 right-0'>👋</span>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'tween',
+              duration: 0.2,
+            }}
+          >
+            <Image
+              alt="profil"
+              src="/profil.png"
+              height="100"
+              width="100"
+              quality="100"
+              priority={true}
+              className="h-36 w-36 object-cover rounded-full border-[0.35rem] border-white shadow-lg"
+            ></Image>
+          </motion.div>
+          <motion.span
+            className="text-4xl absolute bottom-0 right-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
         </div>
-        <p className="text-2xl sm:text-3xl px-10 max-w-3xl text-center leading-relaxed">
-          <span className="font-bold">Hello, I&apos;m Ricardo.</span> I&apos;m a{' '}
-          <span className="font-bold">full-stack developer</span> with{' '}
-          <span className="font-bold">8 years</span> of experience. I enjoy
-          building sites & apps. My focus is{' '}
-          <span className="underline">React (Next.js)</span>.
-        </p>
-        <ul className="flex justify-center items-center flex-col sm:flex-row gap-4 mt-4 max-w-2xl">
-          <motion.li
-            initial="initial"
-            animate="initial"
-            whileHover="animate"
-            variants={bounce}
-          >
-            <Link
-              href="#"
-              className="flex justify-center items-center gap-x-3 text-xl font-medium py-3 px-5 rounded-full text-slate-100 bg-slate-950"
-            >
-              Contact me here{' '}
-              <motion.span variants={arrow}>
-                {' '}
-                <BsArrowRight />
-              </motion.span>
-            </Link>
-          </motion.li>
-          <motion.li
-            initial="initial"
-            animate="initial"
-            whileHover="animate"
-            variants={bounce}
-          >
-            <Link
-              href="#"
-              className="flex justify-center items-center gap-x-3 text-xl font-medium py-3 px-5 rounded-full text-slate-950 bg-slate-100"
-            >
-              Download CV{' '}
-              <motion.span variants={download}>
-                {' '}
-                <LiaDownloadSolid />
-              </motion.span>
-            </Link>
-          </motion.li>
-          <motion.li
-            initial="initial"
-            animate="initial"
-            whileHover="animate"
-            variants={bounce}
-          >
-            <Link
-              href="#"
-              className="text-xl font-medium py-3 px-5 rounded-full text-slate-950 bg-slate-50 flex justify-center items-center"
-            >
-              <BsLinkedin />
-            </Link>
-          </motion.li>
-          <motion.li
-            initial="initial"
-            animate="initial"
-            whileHover="animate"
-            variants={bounce}
-          >
-            <Link
-              href="#"
-              className="text-xl font-medium py-3 px-5 rounded-full text-slate-950 bg-slate-50 flex justify-center items-center"
-            >
-              <AiFillGithub />
-            </Link>
-          </motion.li>
-        </ul>
       </div>
+      <motion.p
+        className="text-2xl sm:text-4xl mb-10 mt-4 px-4 text-center font-medium !leading-[1.5]"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I&apos;m Ricardo.</span> I&apos;m a{' '}
+        <span className="font-bold">full-stack developer</span> with{' '}
+        <span className="font-bold">8 years</span> of experience. I enjoy
+        building sites & apps. My focus is{' '}
+        <span className="underline">React (Next.js)</span>.
+      </motion.p>
+      <motion.ul
+        className="flex justify-center items-center flex-col sm:flex-row gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <motion.li
+          initial="initial"
+          animate="initial"
+          whileHover="animate"
+          variants={bounce}
+        >
+          <Link
+            href="#contact"
+            className="flex items-center gap-2 py-3 px-7 rounded-full text-white bg-gray-900"
+          >
+            Contact me here{' '}
+            <motion.span variants={arrow}>
+              {' '}
+              <BsArrowRight />
+            </motion.span>
+          </Link>
+        </motion.li>
+        <motion.li
+          initial="initial"
+          animate="initial"
+          whileHover="animate"
+          variants={bounce}
+        >
+          <a
+            href="#"
+            className="flex items-center gap-2 py-3 px-7 rounded-full bg-white"
+          >
+            Download CV{' '}
+            <motion.span variants={download}>
+              {' '}
+              <LiaDownloadSolid />
+            </motion.span>
+          </a>
+        </motion.li>
+        <motion.li
+          initial="initial"
+          animate="initial"
+          whileHover="animate"
+          variants={bounce}
+        >
+          <a
+            href="https://www.linkedin.com/feed/"
+            className="flex items-center p-4 rounded-full bg-white text-gray-700"
+          >
+            <BsLinkedin />
+          </a>
+        </motion.li>
+        <motion.li
+          initial="initial"
+          animate="initial"
+          whileHover="animate"
+          variants={bounce}
+        >
+          <a
+            href="https://github.com/IlyasElMabrouki/"
+            className="flex items-center p-4 rounded-full bg-white text-gray-700"
+          >
+            <AiFillGithub />
+          </a>
+        </motion.li>
+      </motion.ul>
     </section>
   );
 }
