ba68691015ceb759e795ce9f9b4a0dbe15eb6eb9
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-11-12 15:20:53 +0100

    Implemented footer component

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="text-center text-gray-500 mb-10 px-4">
+      <small className="mb-2 block text-xs">
+        &copy; 2023 Ilyas El Mabrouki. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">About this website:</span> built with React
+        & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
+        Framer Motion, React Email & Resend.
+      </p>
+    </footer>
+  );
+}
